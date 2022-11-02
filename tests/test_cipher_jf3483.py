ca89a09de2d04e2b059cfd5a84c03b8802124644
JingWen Fu <jf3483@columbia.edu>
2022-11-01 20:30:04 -0400

    test

--- tests/test_cipher_jf3483.py
+++ tests/test_cipher_jf3483.py
@@ -1,1 +1,8 @@
 from cipher_jf3483 import cipher_jf3483
+import pytest
+
+def test_cipher_single():
+    example = 'QMSS'
+    expected = 'RNTT'
+    actual = cipher_jf3483.cipher('QMSS', shift = 1)
+    assert actual == expected
